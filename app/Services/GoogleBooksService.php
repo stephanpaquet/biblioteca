cf3bf001d5e76a7946e6c1ed6eb94ed669d79255
Stéphan Paquet <spaquet@flyscan.com>
2025-06-22 19:33:07 -0400

    feat: Add Google Books API integration with service class and environment configuration

---
+++ app/Services/GoogleBooksService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class GoogleBooksService
+{
+    protected string $baseUrl = 'https://www.googleapis.com/books/v1/volumes';
+    protected ?string $apiKey;
+
+    public function __construct()
+    {
+        $this->apiKey = config('services.google_books.api_key');
+    }
+
+    /**
+     * Search for books using the Google Books API.
+     *
+     * @param string $query
+     * @param int $maxResults
+     * @return array|null
+     */
+    public function searchBooks(string $query, int $maxResults = 10): ?array
+    {
+        $params = [
+            'q' => $query,
+            'maxResults' => $maxResults,
+        ];
+        if ($this->apiKey) {
+            $params['key'] = $this->apiKey;
+        }
+
+        $response = Http::get($this->baseUrl, $params);
+
+        dump($response->body());
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        return null;
+    }
+}
--- config/services.php
+++ config/services.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -35,4 +34,7 @@ return [
         ],
     ],
 
+    'google_books' => [
+        'api_key' => env('GOOGLE_BOOKS_API_KEY'),
+    ],
 ];
