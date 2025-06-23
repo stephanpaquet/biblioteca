96626bc0a7f119c4ba7a3aae0d253a82f51267c3
Stéphan Paquet <spaquet@flyscan.com>
2025-06-22 20:56:58 -0400

    feat: Add link to return to homepage in the footer

--- resources/views/scribe/index.blade.php
+++ resources/views/scribe/index.blade.php
@@ -69,9 +69,10 @@
             </div>
 
     <ul class="toc-footer" id="toc-footer">
-                    <li style="padding-bottom: 5px;"><a href="{{ route("scribe.postman") }}">View Postman collection</a></li>
-                            <li style="padding-bottom: 5px;"><a href="{{ route("scribe.openapi") }}">View OpenAPI spec</a></li>
-                <li><a href="http://github.com/knuckleswtf/scribe">Documentation powered by Scribe ✍</a></li>
+                    <li style="padding-bottom: 5px;"><a href="{{ route('scribe.postman') }}">View Postman collection</a></li>
+        <li style="padding-bottom: 5px;"><a href="{{ route('scribe.openapi') }}">View OpenAPI spec</a></li>
+        <li style="padding-bottom: 5px;"><a href="/">Back to homepage</a></li>
+        <li><a href="http://github.com/knuckleswtf/scribe">Documentation powered by Scribe ✍</a></li>
     </ul>
 
     <ul class="toc-footer" id="last-updated">
