4371bbf3e20f33cda2342f2a6b52372713b3fead
stephan paquet <stephanpaquet@gmail.com>
2025-06-30 05:42:21 -0400

    fix: Correct import path for Layout component in Book.vue

--- resources/js/Pages/Book.vue
+++ resources/js/Pages/Book.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Layout from './Layout.vue';
+import Layout from '../Layouts/Layout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 import { ref, onMounted } from 'vue';
 import { usePage } from '@inertiajs/inertia-vue3';
