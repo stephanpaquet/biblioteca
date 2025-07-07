c8800fbefb22a6008c8e2461c6d57867a7eeff03
Stéphan Paquet <spaquet@flyscan.com>
2025-07-07 10:24:11 -0400

    feat: Remove debug dump statements from SetLocale middleware for cleaner code

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -16,18 +16,15 @@ class SetLocale
 
         // If locale is provided and supported, set it
         if ($locale && in_array($locale, config('app.supported_locales', ['fr']))) {
-            dump('Setting locale from request parameter:', $locale);
             App::setLocale($locale);
             Session::put('locale', $locale);
         }
         // Otherwise, use session locale if available
         elseif (Session::has('locale') && in_array(Session::get('locale'), config('app.supported_locales', ['fr']))) {
             App::setLocale(Session::get('locale'));
-            dump('Setting locale from session:', Session::get('locale'));
         }
         // Fall back to default locale
         else {
-            dump('Setting default locale:', config('app.locale', 'fr'));
             App::setLocale(config('app.locale', 'fr'));
         }
 
