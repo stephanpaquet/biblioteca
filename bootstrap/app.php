ed66db0ff870b855a585c53f3b1e1891ef36edbe
Stéphan Paquet <spaquet@flyscan.com>
2025-07-08 11:22:41 -0400

    refactor: Adjust middleware order in app configuration and clean up LanguageSwitcher component

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
             \App\Http\Middleware\SetLocale::class,
+            \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
--- resources/js/Components/LanguageSwitcher.vue
+++ resources/js/Components/LanguageSwitcher.vue
@@ -1,4 +1,3 @@
-
 <script setup>
 import { ref } from 'vue';
 import { router } from '@inertiajs/vue3';
@@ -9,34 +8,38 @@ const translationsStore = useTranslationsStore();
 const currentLocale = ref(translationsStore.translations.locale);
 
 const languageNames = {
-  en: 'English',
-  fr: 'Français',
-  es: 'Español',
-  de: 'Deutsch'
+    en: 'English',
+    fr: 'Français',
+    es: 'Español',
+    de: 'Deutsch'
 };
 
 function getLanguageName(locale) {
-  return languageNames[locale] || locale.toUpperCase();
+    return languageNames[locale] || locale.toUpperCase();
 }
 
 function changeLocale(event) {
-  router.get(window.location.pathname, { locale: translationsStore.translations.currentLocale }, {
-    preserveState: true,
-    replace: true
-  });
+    router.get(window.location.pathname, { locale: translationsStore.translations.currentLocale }, {
+        preserveState: true,
+        replace: true,
+        onSuccess: (response) => {
+            translationsStore.setTranslations({
+                currentLocale: response.props.currentLocale,
+                supportedLocales: response.props.supportedLocales,
+                texts: response.props.translations
+            });
+        }
+    });
 }
 </script>
 
 <template>
-  <div class="relative">
-    <select
-      v-model="translationsStore.translations.currentLocale"
-      @change="changeLocale"
-      class="appearance-none bg-white border border-gray-300 rounded px-3 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-    >
-      <option v-for="locale in translationsStore.translations.supportedLocales" :key="locale" :value="locale">
-        {{ getLanguageName(locale) }}
-      </option>
-    </select>
-  </div>
+    <div class="relative">
+        <select v-model="translationsStore.translations.currentLocale" @change="changeLocale"
+            class="appearance-none bg-white border border-gray-300 rounded px-3 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+            <option v-for="locale in translationsStore.translations.supportedLocales" :key="locale" :value="locale">
+                {{ getLanguageName(locale) }}
+            </option>
+        </select>
+    </div>
 </template>
